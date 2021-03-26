e2477df44731362a1faf68d83cbb0d240eb79095
jmd0711 <35779500+jmd0711@users.noreply.github.com>
2021-03-26 14:03:07 -0700

    profile class for data

--- profile.cpp
+++ profile.cpp
@@ -1,5 +1,33 @@
 #include "profile.h"
 
+Team::Team(const QJsonObject &newTeam)
+{
+    area_m = newTeam["battle"].toString();
+    teamName_m = newTeam["name"].toString();
+    teamDescription_m = newTeam["description"].toString();
+
+//    QJsonArray uuids = newTeam["uuid"].toArray();
+//    foreach (const QJsonValue &value, uuids)
+//    {
+//        //monsterIndexes_m.push_back(value.toInt());
+//    }
+}
+
+Team::Team(QString teamArea) :
+    area_m{ teamArea }
+{
+    teamName_m = "New Team";
+    teamDescription_m = "";
+}
+
+Team::~Team()
+{
+    foreach (Monster *mon, monsters_m)
+    {
+        removeMonster(mon);
+    }
+}
+
 void Team::addMonster(Monster *monster)
 {
     monsters_m.push_back(monster);
@@ -12,6 +40,36 @@ void Team::removeMonster(Monster *monster)
     monster->removeTeam(this);
 }
 
+Monster::Monster(const QJsonObject &newMonster)
+{
+    accuracy_m = newMonster["accuracy"].toInt();
+    attack_m = newMonster["attack"].toInt();
+    awakened_m = newMonster["awakened"].toBool();
+    critDamage_m = newMonster["criticalDamage"].toInt();
+    critRate_m = newMonster["criticalRate"].toInt();
+    defense_m = newMonster["defense"].toInt();
+    description_m = newMonster["description"].toString();
+    element_m = newMonster["element"].toString();
+    hp_m = newMonster["hp"].toInt();
+    id_m = newMonster["id"].toInt();
+    imagePath_m = newMonster["imagePath"].toString();
+    level_m = newMonster["level"].toInt();
+    name_m = newMonster["name"].toString();
+    priority_m = newMonster["priority"].toInt();
+    resistance_m = newMonster["resistance"].toInt();
+    speed_m = newMonster["speed"].toInt();
+    stars_m = newMonster["stars"].toInt();
+    uuid_m = newMonster["uuid"].toString();
+}
+
+Monster::~Monster()
+{
+    foreach (Team *team, teams_m)
+    {
+        team->removeMonster(this);
+    }
+}
+
 void Monster::addTeam(Team *team)
 {
     teams_m.push_back(team);
@@ -22,7 +80,40 @@ void Monster::removeTeam(Team *team)
     teams_m.erase(std::remove(teams_m.begin(), teams_m.end(), team), teams_m.end());
 }
 
+QString Monster::getUuid_m() const
+{
+    return uuid_m;
+}
+
 Profile::Profile()
 {
 
+}
+
+void Profile::loadProfile(QJsonDocument &doc)
+{
+    QJsonObject obj = doc.object();
+
+    //  Load Monsters
+    //
+    QJsonArray monArray = obj["monsters"].toArray();
+    foreach (const QJsonValue &value, monArray)
+    {
+        Monster *mon = new Monster(value.toObject());
+        monsters_m.push_back(mon);
+    }
+
+    //  Load Teams
+    //
+    QJsonArray teamArray = obj["teams"].toArray();
+    foreach (const QJsonValue &value, teamArray)
+    {
+        Team *team = new Team(value.toObject());
+        QJsonArray uuids = value.toObject()["uuids"].toArray();
+        foreach (Monster *mon, monsters_m)
+        {
+            if (uuids.contains(mon->getUuid_m()))
+                team->addMonster(mon);
+        }
+    }
 }
--- profile.h
+++ profile.h
@@ -1,57 +1,75 @@
 #ifndef PROFILE_H
 #define PROFILE_H
 
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
 #include <QObject>
-#include <QPixmap>
+#include <QImage>
 
 class Monster;
 
 class Team
 {
 public:
-    Team();
+    Team(const QJsonObject &newTeam);
+    Team(QString teamArea);
     ~Team();
 
     void addMonster(Monster *monster);
     void removeMonster(Monster *monster);
 
 private:
+    QString area_m;
+    QString teamName_m;
+    QString teamDescription_m;
     QVector<Monster *> monsters_m;
 };
 
 class Monster
 {
 public:
-    Monster();
+    Monster(const QJsonObject &newMonster);
     ~Monster();
 
     friend void Team::addMonster(Monster *monster);
     friend void Team::removeMonster(Monster *monster);
 
+//private slots:
+//    void onDataReceived(QNetworkReply *reply);
+
+//    void onImageReceived(QNetworkReply *reply);
+
+//signals:
+//    void updated(int index);
+
 private:
     void addTeam(Team *team);
     void removeTeam(Team *team);
+
+//    void requestData();
+//    void requestImage();
 
     QString name_m;
     QString imagePath_m;
     QString element_m;
     QString description_m;
-    bool awakened_m;
-    int id_m;
-    int stars_m;
-    int level_m;
-    int hp_m;
-    int attack_m;
-    int defense_m;
-    int speed_m;
-    int critRate_m;
-    int critDamage_m;
-    int resistance_m;
-    int accuracy_m;
-    int priority_m;
-
-    int index_m;
-    QPixmap image_m;
+    QString uuid_m;
+    bool    awakened_m;
+    int     id_m;
+    int     stars_m;
+    int     level_m;
+    int     hp_m;
+    int     attack_m;
+    int     defense_m;
+    int     speed_m;
+    int     critRate_m;
+    int     critDamage_m;
+    int     resistance_m;
+    int     accuracy_m;
+    int     priority_m;
 
     QVector<Team *> teams_m;
 };
@@ -61,6 +79,8 @@ class Profile
 public:
     Profile();
     ~Profile();
+
+    void loadProfile(QJsonDocument &doc);
 
 private:
     QVector<Monster *> monsters_m;
